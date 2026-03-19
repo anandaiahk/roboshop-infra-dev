8708c755de5a63c393933002ed8052558b537d34
anand k <kanandaiah9@gmail.com>
2026-03-19 18:11:11 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -46,6 +46,15 @@ resource "aws_security_group_rule" "redis_bastion" {
   # give bastion security group
   security_group_id = local.redis_sg_id
 }
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  # give bastion security group
+  security_group_id = local.mysql_sg_id
+}
 
 
 
