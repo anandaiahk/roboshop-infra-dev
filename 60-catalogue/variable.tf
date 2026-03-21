5a77e8ece3b70cb13458f81de79df52494b1c5ab
anand k <kanandaiah9@gmail.com>
2026-03-21 10:30:34 +0530

    terraform

---
+++ 60-catalogue/variable.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0243750UF68Z7UBMQ4E"
+
+}
+
+variable "domain_name" {
+    default = "anand88b.online"
+}
+
+variable "app_version" {
+    default = "v3"
+}
