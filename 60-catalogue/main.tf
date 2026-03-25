b8d6d7c3d182899d92adc3a6728a4e40a86e7764
anand k <kanandaiah9@gmail.com>
2026-03-25 11:17:41 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -188,6 +188,7 @@ resource "aws_lb_listener_rule" "catalogue" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.catalogue.arn
   }
+  depends_on = [aws_lb.backend_alb.arn]
 
   condition {
     host_header {
